ba71389b5e2a06253e8f2bf43ea80b84fdfa54d1
dharanshetty05 <dharanshetty05@gmail.com>
2026-08-16 11:21:02 +0530

    feat: added Express and created server application entry

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,7 @@
-console.log("Backend is running");
+import app from "./app.js";
+
+const PORT = Number(process.env.PORT);
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT ${PORT}`);
+});
